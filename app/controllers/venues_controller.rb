e700132bcb2703c37fc3999d9cb0d8a459c97dd7
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-22 15:30:57 +0000

    added update functionality to airvnp

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,5 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show, :edit, :update, :destroy]
 
   def index
     @venues = Venue.all
@@ -22,10 +23,23 @@ class VenuesController < ApplicationController
     end
   end
 
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+    redirect_to venue_path(@venue)
+  end
+
   private
+
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
 
   def venue_params
     params.require(:venue).permit(:name, :address, :capacity, booked: false)
   end
-
 end
