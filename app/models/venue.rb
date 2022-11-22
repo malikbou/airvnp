3ac63ea5933b63575344e7b4b0675691fee1e526
Chirantan Sahasrabudhe <chirantanvs@hotmail.co.uk>
2022-11-22 14:57:15 +0000

    updated venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,8 @@
 class Venue < ApplicationRecord
   belongs_to :user, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, :address, :capacity, presence: true
   validates :name, :address, uniqueness: { case_sensitive: false }
+  validates :capacity, numericality: { only_integer: true }
 end
