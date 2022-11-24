a2c6d4e02c67a9cea2aae2d51500dc70a6941e93
Chirantan <45467698+ChirantanS@users.noreply.github.com>
2022-11-24 16:57:52 +0000

    changed date to date and time

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_123625) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_164453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_123625) do
     t.bigint "venue_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "start_date"
-    t.date "end_date"
+    t.datetime "start_date", precision: nil
+    t.datetime "end_date", precision: nil
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
@@ -65,11 +64,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_123625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
     t.string "description"
     t.string "category"
     t.decimal "pricing"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
