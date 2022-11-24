57c8a6b86909723a2bc8be5b30d7ac95c0a4677f
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-24 14:42:14 +0000

    got mapbox to work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_163655) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_115607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_163655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
